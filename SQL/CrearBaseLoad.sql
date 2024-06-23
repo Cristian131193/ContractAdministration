aeada30bafc51b749560b010a314c10d24785701
Cristian Alvarado <c_alvarado_cruz@hotmail.com>
2024-06-23 16:30:50 -0600

    Creacion Proyecto SSIS

---
+++ SQL/CrearBaseLoad.sql
@@ -0,0 +1,44 @@
+USE master
+GO
+
+CREATE DATABASE GASTOSPERSONALES_LOAD
+GO
+
+USE GASTOSPERSONALES_LOAD
+GO
+
+CREATE TABLE INGRESOS(
+	ID INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
+	Fecha datetime NULL,
+	Descrip nvarchar(60) NULL,
+	Monto money,
+	ETLLoad datetime,
+	ETLExecution int	
+	)
+GO
+
+
+CREATE TABLE GASTOS(
+	ID INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
+	Fecha datetime NULL,
+	Descrip nvarchar(60) NULL,
+	Monto money,
+	ETLLoad datetime,
+	ETLExecution int	
+	)
+GO
+
+
+CREATE TABLE METAS(
+	ID INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
+	Fecha datetime NULL,
+	Descrip nvarchar(60) NULL,
+	Monto money,
+	ETLLoad datetime,
+	ETLExecution int	
+	)
+GO
+
+
+
+
