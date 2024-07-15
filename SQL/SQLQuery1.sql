196a7b2899b1bde6dc3b789efc0605a201d01481
Cristian Alvarado <c_alvarado_cruz@hotmail.com>
2024-07-14 19:11:41 -0600

    Creacion Dim Tiempo

--- SQL/SQLQuery1.sql
+++ SQL/SQLQuery1.sql
@@ -8,15 +8,33 @@ GO
 
 
 
+CREATE TABLE [GASTOSPERSONALES_STAGE].[dbo].[Dim_Ingresos_Mod](
+	[Ingresos_MOd_ID] [int] IDENTITY(1,1) NOT NULL,
+	[Ingresos_BK] [int] NOT NULL,
+	[Ingresos_Descrip] [nvarchar](60) NULL,
+	[ETLLoad] datetime
+)
+GO
 
-UPDATE M
-SET M.Gastos_Descrip =S.Descrip
-FROM [GASTOSPERSONALES_DATAMART].[dbo].[Dim_Gastos] M
-JOIN [GASTOSPERSONALES_STAGE].[dbo].[GASTOS] S
-ON M.Gastos_BK = S.ID
-WHERE M.Gastos_SK = ?
+
+CREATE TABLE [GASTOSPERSONALES_STAGE].[dbo].[Dim_Metas_Mod](
+	[Metas_MOd_ID] [int] IDENTITY(1,1) NOT NULL,
+	[Metas_BK] [int] NOT NULL,
+	[Metas_Descrip] [nvarchar](60) NULL,
+	[ETLLoad] datetime
+)
+GO
 
 
+CREATE TABLE [GASTOSPERSONALES_STAGE].[dbo].[Dim_Tiempo_Mod](
+	[Tiempo_MOd_ID] [int] IDENTITY(1,1) NOT NULL,
+	[Fecha_BK] [datetime] NOT NULL,
+	[Tiempo_Anno] [nvarchar](4) NULL,
+	[Tiempo_Mes] [nvarchar](2) NULL,
+	[Tiempo_Dia] [nvarchar](2) NULL,
+	[ETLLoad] datetime
+)
+GO
 
 
 
